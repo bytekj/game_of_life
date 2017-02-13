2f54712d46562742f5bb8ecbec5c53d58f65e00b
Kiran Khaladkar <kj.khaladu@gmail.com>
2017-02-13 11:53:29 +0530

    tests for check_surrounding function

--- test_sparse_matrix.py
+++ test_sparse_matrix.py
@@ -1,7 +1,50 @@
-import sparsematrix
+#!/usr/bin/python3
+"""
+Test suite for SparseMatrix Class
+"""
+
 import unittest
-import sys
+from sparsematrix import SparseMatrix
 
 class TestSparseMatrix(unittest.TestCase):
-    def test_check_surrounding(self):
-        pass
+    """
+    Test cases for live/dead cells with one or more live neighbour cells
+    """
+    def check_surrounding_two(self):
+        """
+        test for cell with two live neighbours
+        """
+        matrix = SparseMatrix()
+        matrix.add_element((4, 6), 1)
+        matrix.add_element((5, 6), 1)
+        matrix.add_element((6, 6), 1)
+        self.assertEqual(matrix.check_surrounding((5, 6)), 2)
+
+    def check_surrounding_one(self):
+        """
+        Test for cell with one live neighbour
+        """
+        matrix = SparseMatrix()
+        matrix.add_element((4, 6), 1)
+        matrix.add_element((5, 6), 1)
+        matrix.add_element((6, 6), 1)
+        self.assertEqual(matrix.check_surrounding((4, 6)), 1)
+
+    def check_two_surrounding_dead_cell(self):
+        """
+        Test for dead cell surrounded by two live cells
+        """
+        matrix = SparseMatrix()
+        matrix.add_element((4, 6), 1)
+        matrix.add_element((5, 6), 1)
+        self.assertEqual(matrix.check_surrounding((4, 5)), 2)
+
+    def check_one_surrounding_dead_cell(self):
+        """
+        Test for dead cell surrounded by one live cell
+        """
+        matrix = SparseMatrix()
+        matrix.add_element((4, 6), 1)
+
+        self.assertEqual(matrix.check_surrounding((4, 5)), 1)
+
