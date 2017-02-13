afa05daf82a8474bcb056410872e0208a2051291
Kiran Khaladkar <kj.khaladu@gmail.com>
2017-02-13 12:16:03 +0530

    testing functions

--- test_sparse_matrix.py
+++ test_sparse_matrix.py
@@ -10,7 +10,7 @@ class TestSparseMatrix(unittest.TestCase):
     """
     Test cases for live/dead cells with one or more live neighbour cells
     """
-    def check_surrounding_two(self):
+    def test_check_surrounding_two(self):
         """
         test for cell with two live neighbours
         """
@@ -20,7 +20,7 @@ class TestSparseMatrix(unittest.TestCase):
         matrix.add_element((6, 6), 1)
         self.assertEqual(matrix.check_surrounding((5, 6)), 2)
 
-    def check_surrounding_one(self):
+    def test_check_surrounding_one(self):
         """
         Test for cell with one live neighbour
         """
@@ -30,7 +30,7 @@ class TestSparseMatrix(unittest.TestCase):
         matrix.add_element((6, 6), 1)
         self.assertEqual(matrix.check_surrounding((4, 6)), 1)
 
-    def check_two_surrounding_dead_cell(self):
+    def test_check_two_surrounding_dead_cell(self):
         """
         Test for dead cell surrounded by two live cells
         """
@@ -39,7 +39,7 @@ class TestSparseMatrix(unittest.TestCase):
         matrix.add_element((5, 6), 1)
         self.assertEqual(matrix.check_surrounding((4, 5)), 2)
 
-    def check_one_surrounding_dead_cell(self):
+    def test_check_one_surrounding_dead_cell(self):
         """
         Test for dead cell surrounded by one live cell
         """
@@ -51,4 +51,3 @@ class TestSparseMatrix(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
